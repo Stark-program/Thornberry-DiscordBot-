de08fdc299079679ed84f23f7c63b149e6cd0dfb
Chad Stark <chad.d.stark@hotmail.com>
2022-05-24 09:52:07 -0700

    overhauling the bot. Starting from scratch with discordjs v13

--- bot.js
+++ bot.js
@@ -1,57 +1,27 @@
-const Discord = require("discord.js");
-const path = require("path");
+const { Client, Intents } = require("discord.js");
 require("dotenv").config();
 
-const client = new Discord.Client();
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.on("ready", () => {
+// WHAT WE NEED DYNAMICALLY, FOR THIS BOT TO WORK IN EVERY DISCORD.
+// USER ID OF USERS WE WANT THE BOT TO INTERUPT
+// GUILD ID'S
+// CHANNEL ID'S
+
+client.on("ready", (guild) => {
   console.log("I am ready!");
 });
 
-client.on("voiceStateUpdate", (oldMember, newMember) => {
-  let newUserChannel = newMember.channelID;
-  let oldUserChannel = oldMember.channelID;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+  const { commandName } = interaction;
 
-  const authorized = [process.env.A_DISCORD_ID, process.env.C_DISCORD_ID];
-  const talkingUsers = [];
-
-  if (newUserChannel === process.env.DISCORD_CHANNEL_ID) {
-    const channel = client.channels.cache.get(process.env.DISCORD_CHANNEL_ID);
-    if (!channel) {
-      return console.log("The channel does not exist");
-    }
-    channel
-      .join()
-      .then((connection) => {
-        const audio = {
-          status: false,
-          dispatcher: function () {
-            return connection.play(
-              path.join(__dirname, "thornberry.audio.mp3")
-            );
-          },
-        };
-        client.on("guildMemberSpeaking", (newMember, speaking) => {
-          if (speaking.equals(1) && !authorized.includes(newMember.id)) {
-            console.log(newMember.displayName + " is speaking!");
-            audio["status"] = true;
-            if ((audio["status"] = true)) {
-              return audio.dispatcher();
-            }
-          }
-
-          if (speaking.equals(0) && !authorized.includes(newMember.id)) {
-            audio["status"] = false;
-            console.log(newMember.displayName + " stopped talking");
-            connection.play(path.join(__dirname, "silence.mp3"));
-          }
-        });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  } else {
-    console.log("Left vc");
+  if (commandName === "ping") {
+    await interaction.reply("Pong!");
+  } else if (commandName === "server") {
+    await interaction.reply("Server info.");
+  } else if (commandName === "user") {
+    await interaction.reply("User info.");
   }
 });
 
---
+++ deploy-commands.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+require("dotenv").config();
+
+const guildId = process.env.DISCORD_GUILD_ID;
+const clientId = process.env.DISCORD_APPLICATION_ID;
+const applicationToken = process.env.DISCORD_LOGIN;
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "9" }).setToken(applicationToken);
+
+rest
+  .put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+  .then(() => console.log("Successfully registered application commands."))
+  .catch(console.error);
