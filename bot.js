acc181519b107a9544287e6bfae8dd7fc0ad5bd6
Chad Stark <chad.d.stark@hotmail.com>
2022-05-24 11:21:29 -0700

    setup basic command structure and command handling

--- bot.js
+++ bot.js
@@ -1,28 +1,51 @@
-const { Client, Intents } = require("discord.js");
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Intents } = require("discord.js");
 require("dotenv").config();
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+client.login(process.env.DISCORD_LOGIN);
 
 // WHAT WE NEED DYNAMICALLY, FOR THIS BOT TO WORK IN EVERY DISCORD.
 // USER ID OF USERS WE WANT THE BOT TO INTERUPT
 // GUILD ID'S
 // CHANNEL ID'S
 
-client.on("ready", (guild) => {
+client.on("ready", () => {
   console.log("I am ready!");
 });
 
-client.on("interactionCreate", async (interaction) => {
-  if (!interaction.isCommand()) return;
-  const { commandName } = interaction;
+client.commands = new Collection();
 
-  if (commandName === "ping") {
-    await interaction.reply("Pong!");
-  } else if (commandName === "server") {
-    await interaction.reply("Server info.");
-  } else if (commandName === "user") {
-    await interaction.reply("User info.");
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  // Set a new item in the Collection
+  // With the key as the command name and the value as the exported module
+  client.commands.set(command.data.name, command);
+}
+
+client.on("interactionCreate", async (interaction) => {
+  console.log(interaction.commandName);
+  console.log(client.commands.get(interaction.commandName));
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: "There was an error while executing this command!",
+      ephemeral: true,
+    });
   }
 });
-
-client.login(process.env.DISCORD_LOGIN);
--- deploy-commands.js
+++ deploy-commands.js
@@ -1,4 +1,5 @@
-const { SlashCommandBuilder } = require("@discordjs/builders");
+const fs = require("node:fs");
+const path = require("node:path");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
 require("dotenv").config();
@@ -7,17 +8,18 @@ const guildId = process.env.DISCORD_GUILD_ID;
 const clientId = process.env.DISCORD_APPLICATION_ID;
 const applicationToken = process.env.DISCORD_LOGIN;
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName("ping")
-    .setDescription("Replies with pong!"),
-  new SlashCommandBuilder()
-    .setName("server")
-    .setDescription("Replies with server info!"),
-  new SlashCommandBuilder()
-    .setName("user")
-    .setDescription("Replies with user info!"),
-].map((command) => command.toJSON());
+const commands = [];
+const commandsPath = path.join(__dirname, "commands");
+const commandFiles = fs
+  .readdirSync(commandsPath)
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  console.log(commandFiles, file);
+  const filePath = path.join(commandsPath, file);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: "9" }).setToken(applicationToken);
 
