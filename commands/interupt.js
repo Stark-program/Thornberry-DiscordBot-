82c515982682206d198a15e48430e5b31b61de9d
Chad Stark <chad.d.stark@hotmail.com>
2022-06-21 18:01:48 -0700

    changed to RegEx for checking the mention on the interupt command

--- commands/interupt.js
+++ commands/interupt.js
@@ -1,20 +1,24 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const {
+  MessageMentions: { USERS_PATTERN },
+} = require("discord.js");
 
 const client = require("../client");
 const putItem = require("../dataStorage/putitem");
 
-async function getUserFromMention(mention) {
-  if (!mention) return;
+function getUserFromMention(mention) {
+  console.log(mention);
+  // The id is the first and only match found by the RegEx.
+  const matches = mention.matchAll(USERS_PATTERN).next().value;
 
-  if (mention.startsWith("<@") && mention.endsWith(">")) {
-    mention = mention.slice(2, -1);
+  // If supplied variable was not a mention, matches will be null instead of an array.
+  if (!matches) return;
 
-    if (mention.startsWith("!")) {
-      mention = mention.slice(1);
-    }
-    let user = await client.users.fetch(mention);
-    return user;
-  }
+  // The first element in the matches array will be the entire mention, not just the ID,
+  // so use index 1.
+  const id = matches[1];
+
+  return client.users.cache.get(id);
 }
 
 module.exports = {
@@ -24,16 +28,26 @@ module.exports = {
     .addStringOption((option) => {
       return option
         .setName("name")
-        .setDescription("Tag the user you want interupted. Example: @user")
+        .setDescription(
+          "Tag the user you want interupted (only one). Example: @user"
+        )
         .setRequired(true);
     }),
   async execute(interaction) {
     const userMentiond = interaction.options._hoistedOptions[0].value;
     const user = await getUserFromMention(userMentiond);
+    if (!user) {
+      return interaction.reply({
+        content:
+          "Please enter the user in correctly, with no extra characters. example: @user",
+        ephemeral: true,
+      });
+    }
+    console.log(user);
     const guildId = interaction.member.guild.id;
     const discordId = user.id;
     const username = user.username;
-    //call the function to store the command and its information into the database
-    putItem(guildId, discordId, username);
+    //call the function to store the information into the database
+    // putItem(guildId, discordId, username);
   },
 };
