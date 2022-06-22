5d6d3e48d4af70fb1d2996351abc4b2d2f330f8b
Chad Stark <chad.d.stark@hotmail.com>
2022-06-22 15:36:13 -0700

    dynamoDBClient setup with credentials

--- dataStorage/dynamoclient.js
+++ dataStorage/dynamoclient.js
@@ -1,6 +1,6 @@
 // Create the DynamoDB service client module using ES6 syntax.
 const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
-const AWS = require("aws-sdk");
+// const AWS = require("aws-sdk");
 require("dotenv").config();
 const accessKey = process.env.AWS_ACCESS_KEY_ID;
 const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
@@ -8,13 +8,11 @@ const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 const REGION = "us-west-2"; // For example, "us-east-1".
 // Create an Amazon DynamoDB service client object.
 const ddbClient = new DynamoDBClient({
-  region: REGION,
-});
-const ddb = new AWS.DynamoDB({
-  accessKey,
-  secretAccessKey,
+  credentials: {
+    accessKeyId: accessKey,
+    secretAccessKey: secretAccessKey,
+  },
   region: REGION,
 });
 
 exports.ddbClient = ddbClient;
-exports.ddb = ddb;
